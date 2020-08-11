3f7d456e06e37d2be2e0b408e731046850883b09
leila-a11 <69132221+leila-a11@users.noreply.github.com>
2020-08-11 18:57:30 -0500

    plot 1 code

---
+++ powerTrain.m
@@ -0,0 +1,14 @@
+function [] = powerTrain(C0,C1,C2)
+Pin = 0:5:125;
+Ploss = C0 + C1.*Pin +(C2)*(Pin.^2);
+Pout = Pin - Ploss;
+efficiency = (Pout./Pin);
+max(efficiency);
+figure('numbertitle','off','name','efficiency vs Pin');
+plot(Pin,efficiency);
+title('efficiency vs Pin');
+xlabel ('Pin');
+ylabel ('efficiency');
+
+end
+
