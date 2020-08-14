2f9a3f4e5b532dc6ef2bf0656ff48f109bfb89d1
leila-a11 <69132221+leila-a11@users.noreply.github.com>
2020-08-13 22:57:20 -0500

    powerTrainCode
    This is the code used to plot the graph efficiency vs Pin

--- powerTrain.m
+++ powerTrain.m
@@ -1,5 +1,5 @@
 function [] = powerTrain(C0,C1,C2)
-Pin = 0:5:125;
+Pin = 0:0.1:125;
 Ploss = C0 + C1.*Pin +(C2)*(Pin.^2);
 Pout = Pin - Ploss;
 efficiency = (Pout./Pin);
@@ -9,6 +9,7 @@ plot(Pin,efficiency);
 title('efficiency vs Pin');
 xlabel ('Pin');
 ylabel ('efficiency');
-
+xlim ([0 125]);
+ylim ([0 1]);
 end
 
